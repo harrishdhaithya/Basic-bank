cd37bf541d43bdf0df888b29fe7472335c9cbaa5
harrishdhaithya <harrishdhaithya@gmail.com>
2022-05-05 22:43:06 +0530

    init commit

--- src/MailTest.java
+++
@@ -1,8 +0,0 @@
-import controller.Otp;
-
-public class MailTest {
-    public static void main(String[] args) {
-    	boolean val = Otp.evalOtp("harrishdhaithya@gmail.com");
-    	System.out.println(val);
-    }
-}
